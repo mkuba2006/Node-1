94790c565490afe98531d00300745ba25acbb69b
Jakub Myrta <98383163+mkuba2006@users.noreply.github.com>
2023-02-14 20:11:16 +0100

    1

--- calculator.js
+++ calculator.js
@@ -1,1 +1,9 @@
-//test
+const express = require("express");
+const app = express();
+app.get("/", function (raq, res) {
+  res.send("<h1>Hello Guys</h1>");
+});
+
+app.listen(3000, function () {
+  console.log("started on port 3000");
+});
